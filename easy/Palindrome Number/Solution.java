7730405285bd7e56c3730b16937487f34de73467
janferech <jan.trautwein11@gmail.com>
2023-10-02 20:55:27 +0200

    first commit

---
+++ easy/Palindrome Number/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+        
+        int reverse = 0;
+        int original = x;
+
+        while(x != 0)   {  
+            int remainder = x % 10;  
+            reverse = reverse * 10 + remainder;  
+            x = x/10;  
+        }             
+        return original == reverse;    
+    }
+
+public static void main(String [] args){
+    
+    Scanner sc = new Scanner(System.in);
+        int number = 0;
+
+        System.out.println("Input: ");
+        number = sc.nextInt();
+        sc.close();
+        System.out.println(Solution.isPalindrome(number));
+
+
+
+}
+}
