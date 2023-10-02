c8ffa010436ba3e6b601b0434fb1df2980e939e4
janferech <jan.trautwein11@gmail.com>
2023-10-02 21:21:35 +0200

    roman to integer

---
+++ easy/Roman to Integer/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            switch (currentChar) {
+                case 'I':
+                    if (i < s.length() - 1 && (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X')) {
+                        result -= 1;
+                    } else {
+                        result += 1;
+                    }
+                    break;
+                case 'V':
+                    result += 5;
+                    break;
+                case 'X':
+                    if (i < s.length() - 1 && (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')) {
+                        result -= 10;
+                    } else {
+                        result += 10;
+                    }
+                    break;
+                case 'L':
+                    result += 50;
+                    break;
+                case 'C':
+                    if (i < s.length() - 1 && (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')) {
+                        result -= 100;
+                    } else {
+                        result += 100;
+                    }
+                    break;
+                case 'D':
+                    result += 500;
+                    break;
+                case 'M':
+                    result += 1000;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Ungültige römische Ziffer: " + currentChar);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input: ");
+        String romanNumeral = sc.nextLine();
+        System.out.println(romanToInt(romanNumeral));
+        sc.close();
+    }
+}
