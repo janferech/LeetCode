34c0d2ccfa7e6b12543a38f890858c1ed3aad379
janferech <jan.trautwein11@gmail.com>
2023-10-06 19:37:01 +0200

    format palindrome

--- easy/Palindrome Number/Solution.java
+++ easy/Palindrome Number/Solution.java
@@ -6,21 +6,21 @@ class Solution {
         if (x < 0) {
             return false;
         }
-        
+
         int reverse = 0;
         int original = x;
 
-        while(x != 0)   {  
-            int remainder = x % 10;  
-            reverse = reverse * 10 + remainder;  
-            x = x/10;  
-        }             
-        return original == reverse;    
+        while (x != 0) {
+            int remainder = x % 10;
+            reverse = reverse * 10 + remainder;
+            x = x / 10;
+        }
+        return original == reverse;
     }
 
-public static void main(String [] args){
-    
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         int number = 0;
 
         System.out.println("Input: ");
